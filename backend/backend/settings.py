4b286ce367f80fe2534cfdfe17e5abc72ce8e95b
kareits <mail.igor.li@gmail.com>
2023-12-03 08:04:37 +0600

    Added Backend secret key v2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,9 @@
-import os
+"""Django settings."""
 
-from dotenv import load_dotenv
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
